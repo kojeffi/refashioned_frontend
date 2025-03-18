224037991b01ceb414f22e6fc0d814bdfbe01cc6
kojeffi <omondijeff88@gmail.com>
2025-03-18 16:54:14 +0300

    updates on the live

--- next.config.js
+++ next.config.js
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // ✅ Allow Cloudinary images
-    // domains: ['images.unsplash.com'],
+    domains: ["res.cloudinary.com"], // Allow Cloudinary images
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        pathname: "/dnqsiqqu9/**", // ✅ Allow all images from your Cloudinary account
+        pathname: "/dnqsiqqu9/**", // Allow all images from your Cloudinary account
       },
     ],
   },
   reactStrictMode: true,
+  // Add custom Webpack configuration
+  webpack: (config, { isServer }) => {
+    // Ensure autoprefixer is resolved correctly
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        autoprefixer: require.resolve('autoprefixer'),
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
